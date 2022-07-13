1948f5eaa9b60d4c1ca32e7b8083cb4b046e8ae1
Manuel Kießling <manuel.kiessling@fyyn.io>
2022-07-13 19:25:41 +0200

    Layouting

--- assets/controllers/nav-profile-dropdown-controller.ts
+++ assets/controllers/nav-profile-dropdown-controller.ts
@@ -3,6 +3,9 @@ import { Controller } from '@hotwired/stimulus';
 export default class extends Controller {
     static targets = ['dropdown'];
 
+    isOpen: boolean;
+    dropdownTarget: HTMLButtonElement;
+
     initialize() {
         this.isOpen = false;
     }
--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
-    .addEntry('app', './assets/app.js')
+    .addEntry('app', './assets/app.ts')
 
     .addStyleEntry('tailwind', './assets/styles/tailwind.css')
 
@@ -67,7 +67,7 @@ Encore
     .enableSassLoader()
 
     // uncomment if you use TypeScript
-    //.enableTypeScriptLoader()
+    .enableTypeScriptLoader()
 
     // uncomment if you use React
     //.enableReactPreset()
