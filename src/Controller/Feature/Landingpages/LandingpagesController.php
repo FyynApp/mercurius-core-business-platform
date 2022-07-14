efe37423f414925f16736c44b65235ccf15d75a2
Manuel Kießling <manuel.kiessling@fyyn.io>
2022-07-14 13:16:30 +0200

    Layouting, translations

---
+++ src/Controller/Feature/Landingpages/LandingpagesController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller\Feature\Landingpages;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class LandingpagesController extends AbstractController
+{
+    public function homepageAction(): Response
+    {
+        return $this->render('feature/landingpages/homepage.html.twig');
+    }
+
+    public function featuresAction(): Response
+    {
+        return $this->render('feature/landingpages/features.html.twig');
+    }
+
+    public function pricingAction(): Response
+    {
+        return $this->render('feature/landingpages/pricing.html.twig');
+    }
+}
