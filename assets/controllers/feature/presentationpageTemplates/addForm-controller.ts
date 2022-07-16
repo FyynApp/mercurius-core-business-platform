9afe09cea423cefaa53b75c6dfffccf34daaee9b
Manuel Kießling <manuel.kiessling@fyyn.io>
2022-07-16 23:10:59 +0200

    Refactoring

--- assets/controllers/feature/presentationpageTemplates/addForm-controller.ts
+++ assets/controllers/feature/presentationpageTemplates/addForm-controller.ts
@@ -1,10 +1,18 @@
 import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
-    static targets = ['bgColorHiddenInput', 'bgColorOption'];
+    static targets = [
+        'bgColorHiddenInput', 'bgColorOption', 'bgColorPreview',
+        'textColorHiddenInput', 'textColorOption', 'textColorPreview',
+    ];
 
     bgColorHiddenInputTarget: HTMLInputElement;
     bgColorOptionTargets: HTMLDivElement[];
+    bgColorPreviewTarget: HTMLDivElement;
+
+    textColorHiddenInputTarget: HTMLInputElement;
+    textColorOptionTargets: HTMLDivElement[];
+    textColorPreviewTarget: HTMLDivElement;
 
     selectBgColor(e: Event) {
         const t: HTMLElement = e.target as HTMLElement;
@@ -18,5 +26,23 @@ export default class extends Controller {
         t.classList.add('border-4');
 
         this.bgColorHiddenInputTarget.value = t.dataset['value'];
+
+        this.bgColorPreviewTarget.style.backgroundColor = t.dataset['value'];
+    }
+
+    selectTextColor(e: Event) {
+        const t: HTMLElement = e.target as HTMLElement;
+
+        this.textColorOptionTargets.forEach(e => {
+            e.classList.remove('border-4');
+            e.classList.add('border-0');
+        });
+
+        t.classList.remove('border-0');
+        t.classList.add('border-4');
+
+        this.textColorHiddenInputTarget.value = t.dataset['value'];
+
+        this.textColorPreviewTarget.style.color = t.dataset['value'];
     }
 }
