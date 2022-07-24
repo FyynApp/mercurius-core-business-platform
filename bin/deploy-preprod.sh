84b939ea6d84a0ca40f4edb447d09afa9f7b5c86
Manuel Kießling <manuel.kiessling@fyyn.io>
2022-07-24 13:02:49 +0200

    preprod deployment now deletes files at target that no longer exist at source

--- bin/deploy-preprod.sh
+++ bin/deploy-preprod.sh
@@ -10,5 +10,6 @@ rsync \
   --exclude var/cache/ \
   --exclude var/log/ \
   --exclude public/generated-content/ \
+  --delete \
   "$SCRIPT_FOLDER"/../ \
   www-data@89.58.33.15:/var/www/mercurius-core-business-platform/preprod/
