75dbc3981579d49f09b95c3c640c8526d61398b3
Manuel Kießling <manuel.kiessling@fyyn.io>
2022-07-30 20:25:50 +0200

    Composer & NPM update

--- bin/run-tests.sh
+++ bin/run-tests.sh
@@ -4,7 +4,8 @@ SCRIPT_FOLDER="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )
 
 pushd "$SCRIPT_FOLDER/.."
   rm -rf var/cache/test
-  /usr/bin/env php bin/console --env=test doctrine:database:create --if-not-exists
+  /usr/bin/env php bin/console --env=test doctrine:database:create --if-not-exists --no-interaction
+  /usr/bin/env php bin/console --env=test doctrine:migrations:migrate --no-interaction
   /usr/bin/env php bin/console --env=test doctrine:fixtures:load --no-interaction
   /usr/bin/env php bin/phpunit
 popd
