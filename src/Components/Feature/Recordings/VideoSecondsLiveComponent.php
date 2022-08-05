b95e100798659905e494c571bdb5e6b6e62101ad
Manuel Kießling <manuel.kiessling@fyyn.io>
2022-08-05 22:24:33 +0200

    WIP: dynamic display of video length

--- src/Components/Feature/Recordings/VideoSecondsLiveComponent.php
+++ src/Components/Feature/Recordings/VideoSecondsLiveComponent.php
@@ -5,37 +5,44 @@ namespace App\Components\Feature\Recordings;
 use App\Entity\Feature\Account\User;
 use App\Entity\Feature\Recordings\Video;
 use App\Service\Aspect\DateAndTime\DateAndTimeService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
 use Symfony\UX\LiveComponent\Attribute\LiveProp;
 use Symfony\UX\LiveComponent\DefaultActionTrait;
 
 #[AsLiveComponent('feature_recordings_video_seconds', 'feature/recordings/video_seconds_live_component.html.twig')]
-class VideoSecondsLiveComponent
+class VideoSecondsLiveComponent extends AbstractController
 {
     use DefaultActionTrait;
 
     #[LiveProp]
     public Video $video;
 
-    #[LiveProp]
-    public ?User $user;
-
     public function shouldPoll(): bool
     {
-        if (is_null($this->user)) {
-            throw new AccessDeniedHttpException('No logged in user.');
-        }
+        $isNew = DateAndTimeService::getDateTimeUtc()->getTimestamp() - $this->video->getCreatedAt()->getTimestamp() < 60;
 
-        if ($this->user->getId() !== $this->video->getUser()->getId()) {
-            throw new AccessDeniedHttpException("Video '{$this->video->getId()}' does not belong to user '{$this->user->getId()}'.");
-        }
-
-        if (!is_null($this->video->getAssetFullMp4Seconds()))
-        {
+        if ($isNew && is_null($this->video->getAssetFullMp4Seconds())) {
+            return true;
+        } else {
             return false;
         }
+    }
 
-        return DateAndTimeService::getDateTimeUtc()->getTimestamp() - $this->video->getCreatedAt()->getTimestamp() < 60;
+    public function getSeconds(): ?float
+    {
+        /** @var User $user */
+        $user = $this->getUser();
+
+        if (is_null($user)) {
+            throw new AccessDeniedHttpException('No valid user.');
+        }
+
+        if ($user->getId() !== $this->video->getUser()->getId()) {
+            throw new AccessDeniedHttpException("Video '{$this->video->getId()}' does not belong to logged in user '{$user->getId()}'.");
+        }
+
+        return $this->video->getAssetFullMp4Seconds();
     }
 }
