6672bc4671a220203c9b8416f81c8a0b90c8ef82
Manuel Kießling <manuel.kiessling@fyyn.io>
2022-08-15 08:29:40 +0200

    Layouting

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,20 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
   content: ['./templates/**/*.html.twig', './assets/**/*.ts'],
+  safelist: [
+    'sm:col-span-1',
+    'sm:col-span-2',
+    'sm:col-span-3',
+    'sm:col-span-4',
+    'sm:col-span-5',
+    'sm:col-span-6',
+    'sm:col-span-7',
+    'sm:col-span-8',
+    'sm:col-span-9',
+    'sm:col-span-10',
+    'sm:col-span-11',
+    'sm:col-span-12',
+  ],
   theme: {
     extend: {
       fontFamily: {
