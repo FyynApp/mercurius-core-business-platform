3a86e0662cfee9890f741e73b9bbfe3a6fab1662
Manuel Kießling <manuel.kiessling@fyyn.io>
2022-09-11 21:40:13 +0200

    Neues Layout Präsentationsseite

--- migrations/Version20220910114519.php
+++ migrations/Version20220910114519.php
@@ -17,6 +17,7 @@ final class Version20220910114519 extends AbstractMigration
     public function up(Schema $schema): void
     {
         $this->addSql('ALTER TABLE presentationpage_elements ADD element_horizontal_position VARCHAR(255) NOT NULL');
+        $this->addSql("UPDATE presentationpage_elements SET element_horizontal_position = 'left'");
     }
 
     public function down(Schema $schema): void
