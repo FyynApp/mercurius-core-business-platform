02a0a79da04eb365f2cd6619c7f6e1783f6f4e65
Manuel Kießling <manuel.kiessling@fyyn.io>
2022-09-23 14:59:44 +0200

    WIP: Nicht-registrierte Benutzer
    - Test Case

---
+++ tests/Application/Feature/Recordings/RecordingSessionForBrowserExtensionTest.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Tests\Application\Feature\Recordings;
+
+use App\DataFixtures\Feature\Account\UserFixture;
+use App\Entity\Feature\Recordings\RecordingSession;
+use App\Repository\Feature\Account\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+
+class RecordingSessionForBrowserExtensionTest extends WebTestCase
+{
+    public function testRegisteredUser()
+    {
+        $client = static::createClient();
+        $container = static::getContainer();
+
+        $userRepository = $container->get(UserRepository::class);
+        $user = $userRepository->findOneBy(['email' => UserFixture::TEST_USER_EMAIL]);
+
+        $client->loginUser($user);
+
+        $em = $container->get(EntityManagerInterface::class);
+
+        $this->assertCount(0, $em->getRepository(RecordingSession::class)->findAll());
+
+        $client->request(
+            'POST',
+            '/api/feature/recordings/browser-extension-recording-sessions/'
+        );
+        $this->assertResponseRedirects();
+
+        print_r($client->getResponse()->headers);
+
+        $sessions = $em->getRepository(RecordingSession::class)->findAll();
+
+        $this->assertCount(1, $sessions);
+
+        /** @var RecordingSession $session */
+        $session = $sessions[0];
+
+        $this->assertTrue($session->getUser()->isRegistered());
+
+        $client->followRedirect();
+
+        $structuredResponse = json_decode($client->getResponse()->getContent(), true);
+
+        $this->assertTrue($structuredResponse['settings']['userIsRegistered']);
+        $this->assertTrue($structuredResponse['settings']['userName']);
+    }
+
+    public function testUnregisteredUser()
+    {
+        $client = static::createClient();
+
+        $container = static::getContainer();
+        $em = $container->get(EntityManagerInterface::class);
+
+        $this->assertCount(0, $em->getRepository(RecordingSession::class)->findAll());
+
+        $client->request(
+            'POST',
+            '/api/feature/recordings/browser-extension-recording-sessions/'
+        );
+        $this->assertResponseRedirects();
+
+        $sessions = $em->getRepository(RecordingSession::class)->findAll();
+
+        $this->assertCount(1, $sessions);
+
+        /** @var RecordingSession $session */
+        $session = $sessions[0];
+
+        $this->assertFalse($session->getUser()->isRegistered());
+
+        $client->followRedirect();
+
+        $structuredResponse = json_decode($client->getResponse()->getContent(), true);
+
+        $this->assertFalse($structuredResponse['settings']['userIsRegistered']);
+    }
+}
