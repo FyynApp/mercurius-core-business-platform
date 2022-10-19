525313d6a67f7443c3446a5182f80650dcec6690
Manuel Kießling <manuel.kiessling@fyyn.io>
2022-10-19 09:56:54 +0200

    Fixes

--- src/Controller/Feature/Membership/MembershipController.php
+++ src/Controller/Feature/Membership/MembershipController.php
@@ -3,11 +3,14 @@
 namespace App\Controller\Feature\Membership;
 
 use App\Entity\Feature\Account\User;
+use App\Entity\Feature\Membership\MembershipPlanName;
 use App\Enum\FlashMessageLabel;
 use App\Service\Feature\Membership\MembershipService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Routing\RouterInterface;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
 
@@ -26,6 +29,30 @@ class MembershipController extends AbstractController
                 'availablePlans' => $membershipService->getAvailablePlansForUser($user)
             ]
         );
+    }
+
+    public function subscriptionCheckoutStartAction(
+        string $planName,
+        MembershipService $membershipService,
+        RouterInterface $router
+    ): Response {
+
+        $plan = $membershipService->getMembershipPlanByName(MembershipPlanName::from($planName));
+
+        return $this->redirect($membershipService->getSubscriptionCheckoutUrl(
+            $this->getUser(),
+            $plan,
+            $router->generate(
+                'feature.membership.subscription.checkout.success',
+                [],
+                UrlGeneratorInterface::ABSOLUTE_URL
+            ),
+            $router->generate(
+                'feature.membership.subscription.checkout.cancel',
+                [],
+                UrlGeneratorInterface::ABSOLUTE_URL
+            )
+        ));
     }
 
     public function subscriptionCheckoutSuccessAction(
