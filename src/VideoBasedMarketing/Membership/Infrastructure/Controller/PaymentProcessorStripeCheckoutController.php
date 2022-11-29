66f100b19c33d01f0311165c14261a7dfb3da611
Manuel Kießling <manuel.kiessling@fyyn.io>
2022-11-29 20:12:37 +0100

    Code cleanup

--- src/VideoBasedMarketing/Membership/Infrastructure/Controller/PaymentProcessorStripeCheckoutController.php
+++ src/VideoBasedMarketing/Membership/Infrastructure/Controller/PaymentProcessorStripeCheckoutController.php
@@ -13,7 +13,6 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
