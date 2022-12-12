dae24dc237d383ffedc2a650d6feb850e3fd8844
Manuel Kießling <manuel.kiessling@fyyn.io>
2022-12-12 15:25:16 +0100

    Skript optimizations

--- bin/update-dev-system.sh
+++ bin/update-dev-system.sh
@@ -5,5 +5,5 @@ SCRIPT_FOLDER="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )
 pushd "$SCRIPT_FOLDER/.."
   composer install
   npm install --no-save
-  php bin/console --no-debug doctrine:migrations:migrate
+  php bin/console --no-debug doctrine:migrations:migrate --no-interaction
 popd
