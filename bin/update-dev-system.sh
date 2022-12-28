bd0922e063954b9dcb5e1ec638c17163175cbfd1
Manuel Kießling <manuel.kiessling@fyyn.io>
2022-12-28 17:50:04 +0100

    Ensuring a running Docker container when updating the dev system via script

--- bin/update-dev-system.sh
+++ bin/update-dev-system.sh
@@ -3,6 +3,7 @@
 SCRIPT_FOLDER="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 pushd "$SCRIPT_FOLDER/.."
+  docker start mcbp-db
   composer install
   npm install --no-save
   php bin/console --no-debug doctrine:migrations:migrate --no-interaction
