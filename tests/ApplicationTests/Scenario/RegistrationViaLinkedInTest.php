e4f94e3a0cfbc24e673f57178cf309606a53c208
Manuel Kießling <manuel.kiessling@fyyn.io>
2023-01-15 20:08:46 +0100

    Fix for: full webm asset recorded w unregistered user seems to get "lost" when existing user is claimed
    - WIP

--- tests/ApplicationTests/Scenario/RegistrationViaLinkedInTest.php
+++ tests/ApplicationTests/Scenario/RegistrationViaLinkedInTest.php
@@ -21,7 +21,7 @@ class RegistrationViaLinkedInTest
 
         $client->request(
             'GET',
-            '/account/thirdpartyauth/linkedin/return?code=AQQ4F6tzWOI0f9uPJpI9hunOi3zIiX0A-SyYw5ZcLJvGe21uhkYQZGoQavQjbtGNr7DM7lr7HbzydqOwd2fI-OJ37un8apx8QwQvcwbDQbsokz2VSNciESGH2twb4kwd-SxFpk5VleDWGqnrLlrGNGrZGUU8dtSYLqVw4CFrn3Lg_GxC__eRlQWihA1B8l6qo77n5wIPjFZtrpfEM9o&state=4d4a73452f61cf1ce1879ff95a633436'
+            '/account/thirdpartyauth/linkedin/return'
         );
 
         $this->assertResponseStatusCodeSame(200);
