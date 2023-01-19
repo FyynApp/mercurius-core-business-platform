39abab6dd1df64baa5e6521d7dce2c64807df8cd
Manuel Kießling <manuel.kiessling@fyyn.io>
2023-01-19 19:39:38 +0100

    Provide userLanguage at extension API session request

---
+++ src/VideoBasedMarketing/Account/Presentation/EventSubscriber/UpdateUiLanguageCodeKernelRequestSubscriber.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\VideoBasedMarketing\Account\Presentation\EventSubscriber;
+
+use App\Shared\Domain\Enum\Iso639_1Code;
+use App\Shared\Domain\Service\Iso639_1CodeService;
+use App\VideoBasedMarketing\Account\Domain\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Exception;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+
+
+class UpdateUiLanguageCodeKernelRequestSubscriber
+    implements EventSubscriberInterface
+{
+    private TokenStorageInterface $tokenStorage;
+
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(
+        TokenStorageInterface  $tokenStorage,
+        EntityManagerInterface $entityManager
+    )
+    {
+        $this->tokenStorage = $tokenStorage;
+        $this->entityManager = $entityManager;
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            KernelEvents::REQUEST => [
+                ['handle']
+            ],
+        ];
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function handle(
+        RequestEvent $event
+    ): void
+    {
+        /** @var User $user */
+        $user = $this->tokenStorage->getToken()?->getUser();
+
+        if (is_null($user)) {
+            return;
+        }
+
+        if ($event->getRequestType() === HttpKernelInterface::SUB_REQUEST) {
+            return;
+        }
+
+        $pathInfo = $event->getRequest()->getPathInfo();
+        $pathInfoSegments = explode('/', $pathInfo);
+
+        $languageCode = null;
+
+        if (sizeof($pathInfoSegments) > 1) {
+            $languageCode = Iso639_1Code::tryFrom($pathInfoSegments[1]);
+        }
+
+        if (   is_null($languageCode)
+            && is_null($user->getUiLanguageCode())
+        ) {
+            $languageCode = Iso639_1CodeService::getCodeFromRequest($event->getRequest());
+        }
+
+        if (!is_null($languageCode)) {
+            $user->setUiLanguageCode($languageCode);
+            $this->entityManager->persist($user);
+            $this->entityManager->flush();
+        }
+    }
+}
