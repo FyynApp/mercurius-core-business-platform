e98af3aa49d031ca8305bde16d4ca72dba4e4718
Manuel Kießling <manuel.kiessling@fyyn.io>
2023-01-21 12:24:17 +0100

    Ensure AI does not translate German to English

--- src/VideoBasedMarketing/Mailings/Infrastructure/Service/OpenAiService.php
+++ src/VideoBasedMarketing/Mailings/Infrastructure/Service/OpenAiService.php
@@ -2,16 +2,19 @@
 
 namespace App\VideoBasedMarketing\Mailings\Infrastructure\Service;
 
+use App\Shared\Domain\Enum\Iso639_1Code;
+use App\VideoBasedMarketing\Mailings\Domain\Entity\VideoMailing;
 use Exception;
 use Orhanerday\OpenAi\OpenAi;
+use ValueError;
 
 readonly class OpenAiService
 {
     /**
      * @throws Exception
      */
-    public function improveText(
-        string $text,
+    public function improveTextForVideoMailing(
+        VideoMailing $videoMailing,
         string $promptNotes = '',
     ): string|bool
     {
@@ -19,13 +22,28 @@ readonly class OpenAiService
 
         $openAi = new OpenAi($openAiApiKey);
 
-        $text = mb_ereg_replace("\n", ' ', $text);
+        $text = mb_ereg_replace("\n", ' ', $videoMailing->getBodyAboveVideo());
 
-        $prompt = <<<EOT
+        if (   is_null($videoMailing->getUser()->getUiLanguageCode())
+            || $videoMailing->getUser()->getUiLanguageCode() === Iso639_1Code::En
+        ) {
+            $prompt = <<<EOT
 Based on the following text, please improve it by making it sound professional and more interesting, while keeping it concise and to the point, and without translating it into another language:
 
 $text
 EOT;
+        } elseif (
+            $videoMailing->getUser()->getUiLanguageCode() === Iso639_1Code::De
+        ) {
+            $prompt = <<<EOT
+Bitte optimiere den folgenden Text, indem du ihn professioneller und interessanter formulierst, während du ihn dabei kurz und griffig belässt, und ohne ihn in eine andere Sprache zu übersetzen:
+
+$text
+EOT;
+        } else {
+            throw new ValueError("Cannot handle ui language code '{$videoMailing->getUser()->getUiLanguageCode()->value}'.");
+        }
+
 
         $maxTokens = (int)(mb_strlen($text) / 4);
 
