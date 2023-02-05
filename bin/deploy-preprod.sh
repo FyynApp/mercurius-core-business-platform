6c54ee020a0c82fe7f9fc70454233709eb651ad8
Manuel Kießling <manuel.kiessling@fyyn.io>
2023-02-05 14:54:23 +0100

    Deploy scripts

--- bin/deploy-preprod.sh
+++ bin/deploy-preprod.sh
@@ -25,6 +25,7 @@ rsync \
   --exclude node_modules/ \
   --exclude var/cache/ \
   --exclude var/log/ \
+  --exclude drivers/ \
   --exclude public/generated-content/ \
   --delete \
   "$SCRIPT_FOLDER"/../ \
