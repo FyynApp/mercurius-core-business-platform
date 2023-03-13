f8c177398919cc36cdc70b9f829d679129ebf8a0
Manuel Kießling <manuel.kiessling@fyyn.io>
2023-03-13 12:06:51 +0100

    Integrate Happy Scribe

--- migrations/Version20230313105832.php
+++ migrations/Version20230313105832.php
@@ -7,9 +7,6 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20230313105832 extends AbstractMigration
 {
     public function getDescription(): string
@@ -19,7 +16,6 @@ final class Version20230313105832 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('
             CREATE TABLE audio_transcription_happy_scribe_exports
             (
@@ -158,7 +154,6 @@ final class Version20230313105832 extends AbstractMigration
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE audio_transcription_happy_scribe_exports DROP FOREIGN KEY FK_285DB4A9253B1B25');
         $this->addSql('ALTER TABLE audio_transcription_happy_scribe_transcriptions DROP FOREIGN KEY FK_399005BF7B734100');
         $this->addSql('ALTER TABLE audio_transcription_happy_scribe_translation_tasks DROP FOREIGN KEY FK_2768444253B1B25');
