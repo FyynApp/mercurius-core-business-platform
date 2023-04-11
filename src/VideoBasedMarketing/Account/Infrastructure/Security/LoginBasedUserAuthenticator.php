9e81ef368f1ba85497aec779b7e364bcce27fd7e
Manuel Kießling <manuel.kiessling@fyyn.io>
2023-04-11 20:21:07 +0200

    Redirect to sign in page when trying to access a protected route while not being authenticated

---
+++ src/VideoBasedMarketing/Account/Infrastructure/Security/LoginBasedUserAuthenticator.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\VideoBasedMarketing\Account\Infrastructure\Security;
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Http\EntryPoint\AuthenticationEntryPointInterface;
+
+
+readonly class LoginBasedUserAuthenticator
+    implements AuthenticationEntryPointInterface
+{
+    public function __construct(
+        private UrlGeneratorInterface $urlGenerator
+    )
+    {
+    }
+
+    public function start(
+        Request                 $request,
+        AuthenticationException $authException = null
+    ): Response
+    {
+        return new RedirectResponse(
+            $this->urlGenerator->generate('videobasedmarketing.account.presentation.sign_in')
+        );
+    }
+}
