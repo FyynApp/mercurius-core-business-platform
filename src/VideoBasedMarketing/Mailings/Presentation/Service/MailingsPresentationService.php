3ec96f07bb32461d1e7e161e89e8fe77e1b68187
Manuel Kießling <manuel.kiessling@fyyn.io>
2023-05-17 13:01:10 +0200

    Fix for https://github.com/symfony/ux/issues/881

--- src/VideoBasedMarketing/Mailings/Presentation/Service/MailingsPresentationService.php
+++ src/VideoBasedMarketing/Mailings/Presentation/Service/MailingsPresentationService.php
@@ -4,16 +4,25 @@ namespace App\VideoBasedMarketing\Mailings\Presentation\Service;
 
 use App\Shared\Presentation\Service\MailService;
 use App\VideoBasedMarketing\Mailings\Domain\Entity\VideoMailing;
+use App\VideoBasedMarketing\Recordings\Infrastructure\Enum\AssetMimeType;
+use App\VideoBasedMarketing\Recordings\Infrastructure\Service\RecordingsInfrastructureService;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\DependencyInjection\ParameterBag\ContainerBagInterface;
+use Symfony\Component\Mime\Part\DataPart;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
 readonly class MailingsPresentationService
 {
+    private string $kernelProjectDir;
+
     public function __construct(
-        private MailService         $mailService,
-        private TranslatorInterface $translator,
+        private MailService                     $mailService,
+        private TranslatorInterface             $translator,
+        private RecordingsInfrastructureService $recordingsInfrastructureService,
+        ContainerBagInterface                   $containerBag,
     )
     {
+        $this->kernelProjectDir = $containerBag->get('kernel.project_dir');
     }
 
 
@@ -27,6 +36,22 @@ readonly class MailingsPresentationService
             ->sender($this->mailService->getDefaultSenderAddress())
             ->to($videoMailing->getReceiverMailAddress())
             ->subject("{$this->translator->trans('video_mailing_editor.email.subject_prefix', [], 'videobasedmarketing.mailings')}{$videoMailing->getSubject()}")
+            ->addPart(
+                (
+                    new DataPart(
+                        fopen(
+                            $this->kernelProjectDir
+                                . DIRECTORY_SEPARATOR
+                                . 'public'
+                                . DIRECTORY_SEPARATOR
+                                . 'generated-content'
+                                . DIRECTORY_SEPARATOR
+                            . $this->recordingsInfrastructureService->getVideoPosterStillWithPlayOverlayForEmailAssetRelativeFilePath($videoMailing->getVideo()),
+                            'r'
+                        ), 'videoPreview', AssetMimeType::ImagePng->value
+                    )
+                )->asInline()
+            )
             ->htmlTemplate(
                 '@videobasedmarketing.mailings/video_mailing.email.html.twig'
             );
