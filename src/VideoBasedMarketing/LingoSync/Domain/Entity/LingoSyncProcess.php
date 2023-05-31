d6d1789561e31f0392b25a8c762e69f2a83cceff
Manuel Kießling <manuel.kiessling@fyyn.io>
2023-05-31 21:22:41 +0200

    LingoSync
    - WIP

---
+++ src/VideoBasedMarketing/LingoSync/Domain/Entity/LingoSyncProcess.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\VideoBasedMarketing\LingoSync\Domain\Entity;
+
+use App\Shared\Domain\Enum\Bcp47LanguageCode;
+use App\Shared\Infrastructure\Service\DateAndTimeService;
+use App\VideoBasedMarketing\Organization\Domain\Entity\Organization;
+use App\VideoBasedMarketing\Organization\Domain\Entity\OrganizationOwnedEntityInterface;
+use App\VideoBasedMarketing\Recordings\Domain\Entity\Video;
+use DateTime;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Exception;
+use Symfony\Bridge\Doctrine\IdGenerator\UuidGenerator;
+
+
+#[ORM\Entity]
+#[ORM\Table(name: 'lingosync_processes')]
+#[ORM\Index(
+    fields: ['createdAt'],
+    name: 'created_at_idx'
+)]
+class LingoSyncProcess
+    implements OrganizationOwnedEntityInterface
+{
+    /**
+     * @throws Exception
+     */
+    public function __construct(
+        Video             $video,
+        Bcp47LanguageCode $originalLanguageBcp47LanguageCode
+    )
+    {
+        $this->video = $video;
+        $this->originalLanguageBcp47LanguageCode = $originalLanguageBcp47LanguageCode;
+        $this->createdAt = DateAndTimeService::getDateTime();
+    }
+
+
+    #[ORM\Id]
+    #[ORM\GeneratedValue(strategy: 'CUSTOM')]
+    #[ORM\CustomIdGenerator(class: UuidGenerator::class)]
+    #[ORM\Column(
+        type: Types::GUID,
+        unique: true
+    )]
+    private ?string $id = null;
+
+    public function getId(): ?string
+    {
+        return $this->id;
+    }
+
+
+    #[ORM\Column(
+        type: Types::DATETIME_MUTABLE,
+        nullable: false
+    )]
+    private DateTime $createdAt;
+
+    public function getCreatedAt(): DateTime
+    {
+        return $this->createdAt;
+    }
+
+
+    #[ORM\ManyToOne(
+        targetEntity: Video::class,
+        cascade: ['persist'],
+    )]
+    #[ORM\JoinColumn(
+        name: 'videos_id',
+        referencedColumnName: 'id',
+        nullable: false,
+        onDelete: 'CASCADE'
+    )]
+    private Video $video;
+
+    public function getVideo(): Video
+    {
+        return $this->video;
+    }
+
+
+    #[ORM\Column(
+        type: Types::STRING,
+        length: 16,
+        nullable: false,
+        enumType: Bcp47LanguageCode::class
+    )]
+    private Bcp47LanguageCode $originalLanguageBcp47LanguageCode;
+
+    public function getOriginalLanguageBcp47LanguageCode(): Bcp47LanguageCode
+    {
+        return $this->originalLanguageBcp47LanguageCode;
+    }
+
+    public function getOrganization(): Organization
+    {
+        return $this->video->getOrganization();
+    }
+}
