9848c7c5d66170e00271f38941afe405bf2f5022
Manuel Kießling <manuel.kiessling@fyyn.io>
2023-06-16 10:37:35 +0200

    LingoSync
    - WIP

--- migrations/Version20230616062501.php
+++ migrations/Version20230616062501.php
@@ -16,6 +16,7 @@ final class Version20230616062501 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
+        $this->addSql('DELETE FROM lingosync_processes');
         $this->addSql('ALTER TABLE lingosync_process_tasks ADD result MEDIUMTEXT DEFAULT NULL');
     }
 
